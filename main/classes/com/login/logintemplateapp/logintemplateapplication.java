95b0a640ca2c63ca9e0de1a984de34ee5d8962e1
Caleb Yang <caleb.yanghannchih93@gmail.com>
2024-06-23 13:13:02 +0800

    Create logintemplateapplication.java

---
+++ main/classes/com/login/logintemplateapp/logintemplateapplication.java
@@ -0,0 +1,13 @@
+package com.login.logintemplateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogintemplateappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogintemplateappApplication.class, args);
+	}
+
+}
